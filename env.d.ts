ae4af25c8b50419c750f83d5f68588d8b8063ca0
nothing <zhujianghua@qianmi.com>
2024-03-11 22:47:22 +0800

    add dependence

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,38 @@
 /// <reference types="vite/client" />
+
+declare type Primitive = string | boolean | number | null | undefined | any
+declare type Trivial = string | number
+declare type POJO<T = any> = {
+  [p: string]: T
+}
+declare type POVO = {
+  [p: string]: Primitive
+}
+declare type POTO = {
+  [p: string]: Trivial
+}
+declare type POSO = {
+  [p: string]: string
+}
+declare interface PIJO<T = any> {
+  [x: number]: T
+}
+declare interface PITO<T = Trivial> {
+  [x: number]: T
+}
+declare interface PISO {
+  [x: number]: string
+}
+declare interface PINO {
+  [x: number]: number
+}
+
+declare type TColor = `#${string}`
+
+declare type ThemeType = 'light' | 'dark'
+
+declare type Noop<T = any> = (e?: T) => void
+
+declare type AsyncNoop<T = any> = (e?: T) => Promise<void>
+
+declare type TLanguage = 'zh-CN' | 'en-US'
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import i18 from '~/locale'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(i18)
+// app.use(router)
 
 app.mount('#app')
